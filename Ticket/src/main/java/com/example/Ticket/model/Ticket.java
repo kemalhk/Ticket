bda21a78344525f4adafb98373c58ff56708eff4
kemal <stormanq37@gmail.com>
2023-07-11 13:30:00 +0300

    tek rabbitmq çalışıyor

--- Ticket/src/main/java/com/example/Ticket/model/Ticket.java
+++ Ticket/src/main/java/com/example/Ticket/model/Ticket.java
@@ -3,6 +3,7 @@ package com.example.Ticket.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class Ticket {
     @Id
     private String id;
